3d55c5e88787f98d6bc8639fcee062e6ffc73a02
Ruslan Sakevych <xlionell@gmail.com>
2017-09-09 23:55:49 -0700

    Move git aliases from fish to .gitconfig

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,9 +1,4 @@
-alias gs='git status'
-alias ga='git add'
-alias gc='git commit'
-alias push='git push'
-alias pull='git pull'
-alias gl='git log -10 --oneline'
-
 set -gx GOPATH $HOME/dev/go
 set -gx PATH $PATH $HOME/dev/go/bin
+
+alias blaze "bazel"
